76f9d5de842f9bebe09514480d3ddf7bec5bef59
Kavin <kavinsharma456@gmail.com>
2022-06-15 11:12:22 +0530

    swagger hello world

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const swaggerUi = require("swagger-ui-express"),
+swaggerDocument = require("./swagger.json");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
